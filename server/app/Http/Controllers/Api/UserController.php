64bdc1c8b8fb0e16c8787919fcc2f43697bcc818
crocodiledeathspin <nicklashcargando@gmail.com>
2026-05-04 10:26:20 +0800

    lalalalala

--- server/app/Http/Controllers/Api/UserController.php
+++ server/app/Http/Controllers/Api/UserController.php
@@ -96,16 +96,16 @@ if($request->hasFile('add_user_profile_picture')) {
 
     public function updateUser(Request $request, User $user)
     {
-        $validated = $request->validate([
-            'edit_user_profile_picture' => ['nullable', 'image', 'mimes:png,jpg,jpeg'],
-            'first_name' => ['required', 'max:55'],
-            'middle_name' => ['nullable', 'max:55'],
-            'last_name' => ['required', 'max:55'],
-            'suffix_name' => ['nullable', 'max:55'],
-            'gender' => ['required'],
-            'birth_date' => ['required', 'date'],
-            'username' => ['required', 'min:6', 'max:12', Rule::unique('tbl_users', 'username')->ignore($user)]
-        ]);
+    $validated = $request->validate([
+        'edit_user_profile_picture' => ['nullable', 'image', 'mimes:png,jpg,jpeg'],
+        'first_name' => ['required', 'max:55'],
+        'middle_name' => ['nullable', 'max:55'],
+        'last_name' => ['required', 'max:55'],
+        'suffix_name' => ['nullable', 'max:55'],
+        'gender' => ['required'],
+        'birth_date' => ['required', 'date'],
+        'username' => ['sometimes', 'min:6', 'max:12', Rule::unique('tbl_users', 'username')->ignore($user->user_id, 'user_id')]
+    ]);
 if ($request->hasFile('edit_user_profile_picture')) {
     if ($user->profile_picture && Storage::exists('public/img/user/profile_pictures/' . $user->profile_picture)) {
         Storage::delete('public/img/user/profile_pictures/' . $user->profile_picture);
--- server/routes/api.php
+++ server/routes/api.php
@@ -10,28 +10,27 @@ Route::controller(AuthController::class)->prefix('/auth')->group(function () {
     Route::post('/login', 'login');
 });
 
+Route::controller(GenderController::class)->prefix('/gender')->group(function() {
+    Route::get('/loadGenders', 'loadGenders');
+    Route::get('/getGender/{genderId}', 'getGender');
+    Route::post('/storeGender', 'storeGender');
+    Route::put('/updateGender/{genderId}', 'updateGender');
+    Route::put('/destroyGender/{genderId}', 'destroyGender');
+});
+
+Route::controller(UserController::class)->prefix('/user')->group(function() {
+    Route::get('/loadUsers', 'loadUsers');
+    Route::post('/storeUser', 'storeUser');
+    Route::match(['put', 'post'], '/updateUser/{user_id}', 'updateUser');
+    Route::match(['put', 'post'], '/destroyUser/{user_id}', 'destroyUser');
+});
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::controller(AuthController::class)->prefix('/auth')->group(function () {
         Route::get('/me', 'me');
         Route::post('/logout', 'logout');
     });
-
-    Route::controller(GenderController::class)->prefix('/gender')->group(function () {
-        Route::get('/loadGenders', 'loadGenders'); // /gender/loadGenders
-        Route::get('/getGender/{genderId}', 'getGender');
-        Route::post('/storeGender', 'storeGender'); // /gender/storeGender
-        Route::put('/updateGender/{gender}', 'updateGender');
-        Route::put('/destroyGender/{gender}', 'destroyGender');
-    });
-
-    Route::controller(UserController::class)->prefix('/user')->group(function () {
-        Route::get('/loadUsers', 'loadUsers');
-        Route::post('/storeUser', 'storeUser');
-        Route::put('/updateUser/{user}', 'updateUser');
-        Route::put('/destroyUser/{user}', 'destroyUser');
-    });
 });
-
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
